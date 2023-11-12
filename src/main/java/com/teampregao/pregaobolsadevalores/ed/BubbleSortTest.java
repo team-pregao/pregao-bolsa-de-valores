484e5c4320a2534d119cee3afac2c6cac92b85dd
felipepbovo <126599199+felipepbovo@users.noreply.github.com>
2023-11-12 00:12:56 -0300

    Update BubbleSortTest.java

--- src/main/java/com/teampregao/pregaobolsadevalores/ed/BubbleSortTest.java
+++ src/main/java/com/teampregao/pregaobolsadevalores/ed/BubbleSortTest.java
@@ -1,21 +1,16 @@
 public class BubbleSortTest {
     public static void main(String[] args) {
-        // Criar um array para teste
         int[] array = {64, 34, 25, 12, 22, 11, 90};
 
-        // Exibir o array antes da ordenação
         System.out.println("Array antes da ordenação:");
         printArray(array);
 
-        // Chamar o método de ordenação bubbleSort
         bubbleSort(array);
 
-        // Exibir o array após a ordenação
         System.out.println("\nArray após a ordenação:");
         printArray(array);
     }
 
-    // Método de ordenação bubbleSort (mesmo código do exemplo anterior)
     static void bubbleSort(int[] array) {
         int n = array.length;
         for (int i = 0; i < n - 1; i++) {
@@ -29,7 +24,6 @@ public class BubbleSortTest {
         }
     }
 
-    // Método auxiliar para imprimir o array
     static void printArray(int[] array) {
         int n = array.length;
         for (int i = 0; i < n; ++i) {
