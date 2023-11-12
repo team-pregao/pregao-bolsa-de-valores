289845ca1236cf2f91d371e0fee2b623ac28eaf4
felipepbovo <126599199+felipepbovo@users.noreply.github.com>
2023-11-12 00:06:02 -0300

    Update BubbleSort.java

--- src/main/java/com/teampregao/pregaobolsadevalores/ed/BubbleSort.java
+++ src/main/java/com/teampregao/pregaobolsadevalores/ed/BubbleSort.java
@@ -4,7 +4,6 @@ public class BubbleSort {
         int n = array.length;
         for (int i = 0; i < n - 1; i++) {
             for (int j = 0; j < n - i - 1; j++) {
-                // Se o elemento atual for maior que o próximo, troque-os
                 if (array[j] > array[j + 1]) {
                     // Troca array[j] e array[j+1]
                     int temp = array[j];
