60713c791192f9d2642e4591530df2138c702b20
felipepbovo <126599199+felipepbovo@users.noreply.github.com>
2023-11-12 00:07:39 -0300

    Update BubbleSort.java

--- src/main/java/com/teampregao/pregaobolsadevalores/ed/BubbleSort.java
+++ src/main/java/com/teampregao/pregaobolsadevalores/ed/BubbleSort.java
@@ -1,3 +1,4 @@
+package com.teampregao.pregaobolsadevalores.ed;
 public class BubbleSort {
   
     static void bubbleSort(int[] array) {
@@ -20,3 +21,4 @@ public class BubbleSort {
         }
         System.out.println();
     }
+}
